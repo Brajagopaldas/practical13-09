746426e4bc4c0f492031b617ab2f8327518d06b7
Braja <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-13 06:10:58 -0700

    Add CheckNumber class to evaluate number sign

---
+++ CheckNumber.java
@@ -0,0 +1,13 @@
+class CheckNumber {
+    public static void main (String[] args){
+        int number = -12;
+        
+        if (number > 0) {
+            System.out.println(number + " is a Positive number");
+        } else if (number < 0) {
+           System.out.println(number + " is a Negative number"); 
+        } else {
+            System.out.println(number +" is a Zero");
+        }
+    }
+}
