6d502c59431b21945eee19946095a90ef1020320
Braja <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-15 20:07:45 +0530

    Implement basic voting eligibility check

---
+++ Voting.java
@@ -0,0 +1,13 @@
+class Voting {
+    public static void main(String[] args) {
+        int age = 30;
+        boolean citizen = true;
+
+        if (age >= 18 && citizen == true) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+        
+    }
+}
