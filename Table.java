4a1e8e55d37818536a0433d30b91c550a8cb96c9
Braja <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-15 21:16:28 +0530

    Add multiplication table program in Table.java

---
+++ Table.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Table {
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(number + " x " + (i + 1) + " = " + (number * (i + 1)));
+        }
+    }
+}
