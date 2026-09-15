c9e42658f306166d362a72618e962947c0c95d7b
Braja <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-15 20:13:38 +0530

    Add Divisible class to check divisibility by 5 and 10

---
+++ Divisible.java
@@ -0,0 +1,12 @@
+class Divisible {
+    public static void main(String[] args) {
+        int number = 50;
+        
+        if (number % 5 == 0 && number % 10 == 0 ) {
+            System.out.println("Number is divisible by both 5 and 10");
+        } else {
+            System.out.println("Number is Not divisible by both 5 and 10");
+        }
+        
+    }
+}
