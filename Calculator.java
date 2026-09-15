d07fe2afda15695510d602d8cad017657c26ac02
Braja <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-15 20:43:04 +0530

    Add basic Calculator class with operations

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+class Calculator {
+
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 20;
+        char operator = '*';
+
+        switch (operator) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
+    }
+}
